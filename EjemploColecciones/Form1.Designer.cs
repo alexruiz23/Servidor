d8a2b3a7d58ec46fcb43463de671888486d22151
Alex <alejandroruiztetuan@gmail.com>
2024-10-01 13:14:49 +0200

    SERVIDOR

---
+++ EjemploColecciones/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace EjemploColecciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            lstPaises = new ListBox();
+            groupBox2 = new GroupBox();
+            bttEdit = new Button();
+            bttRemove = new Button();
+            bttAdd = new Button();
+            txtSaludo = new TextBox();
+            label2 = new Label();
+            txtPais = new TextBox();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(lstPaises);
+            groupBox1.Location = new Point(266, 18);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(230, 158);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Lista Paises";
+            // 
+            // lstPaises
+            // 
+            lstPaises.FormattingEnabled = true;
+            lstPaises.ItemHeight = 15;
+            lstPaises.Location = new Point(22, 22);
+            lstPaises.Name = "lstPaises";
+            lstPaises.Size = new Size(187, 109);
+            lstPaises.TabIndex = 0;
+            lstPaises.SelectedIndexChanged += lstPaises_SelectedIndexChanged;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(bttEdit);
+            groupBox2.Controls.Add(bttRemove);
+            groupBox2.Controls.Add(bttAdd);
+            groupBox2.Controls.Add(txtSaludo);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(txtPais);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Location = new Point(18, 18);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(224, 158);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Introduce datos";
+            // 
+            // bttEdit
+            // 
+            bttEdit.Location = new Point(143, 105);
+            bttEdit.Name = "bttEdit";
+            bttEdit.Size = new Size(37, 26);
+            bttEdit.TabIndex = 6;
+            bttEdit.Text = "x";
+            bttEdit.UseVisualStyleBackColor = true;
+            bttEdit.Click += bttEdit_Click;
+            // 
+            // bttRemove
+            // 
+            bttRemove.Location = new Point(94, 105);
+            bttRemove.Name = "bttRemove";
+            bttRemove.Size = new Size(37, 26);
+            bttRemove.TabIndex = 5;
+            bttRemove.Text = "-";
+            bttRemove.UseVisualStyleBackColor = true;
+            bttRemove.Click += bttRemove_Click;
+            // 
+            // bttAdd
+            // 
+            bttAdd.Location = new Point(45, 105);
+            bttAdd.Name = "bttAdd";
+            bttAdd.Size = new Size(37, 26);
+            bttAdd.TabIndex = 4;
+            bttAdd.Text = "+";
+            bttAdd.UseVisualStyleBackColor = true;
+            bttAdd.Click += bttAdd_Click;
+            // 
+            // txtSaludo
+            // 
+            txtSaludo.Location = new Point(63, 57);
+            txtSaludo.Name = "txtSaludo";
+            txtSaludo.Size = new Size(138, 23);
+            txtSaludo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Saludo";
+            // 
+            // txtPais
+            // 
+            txtPais.Location = new Point(63, 28);
+            txtPais.Name = "txtPais";
+            txtPais.Size = new Size(138, 23);
+            txtPais.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(28, 15);
+            label1.TabIndex = 0;
+            label1.Text = "País";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(541, 229);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private ListBox lstPaises;
+        private GroupBox groupBox2;
+        private Button bttEdit;
+        private Button bttRemove;
+        private Button bttAdd;
+        private TextBox txtSaludo;
+        private Label label2;
+        private TextBox txtPais;
+        private Label label1;
+    }
+}
---
+++ Ejercicio2/Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grp1 = new GroupBox();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label2 = new Label();
+            txt2 = new TextBox();
+            label1 = new Label();
+            txt1 = new TextBox();
+            grp1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grp1
+            // 
+            grp1.Controls.Add(button3);
+            grp1.Controls.Add(button2);
+            grp1.Controls.Add(button1);
+            grp1.Controls.Add(label2);
+            grp1.Controls.Add(txt2);
+            grp1.Controls.Add(label1);
+            grp1.Controls.Add(txt1);
+            grp1.Location = new Point(72, 67);
+            grp1.Name = "grp1";
+            grp1.Size = new Size(550, 280);
+            grp1.TabIndex = 0;
+            grp1.TabStop = false;
+            grp1.Text = "tabla de clasificacion";
+            grp1.Enter += grp1_Enter;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(153, 216);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Derrota";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(311, 216);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Victoria";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(225, 170);
+            button1.Name = "button1";
+            button1.Size = new Size(92, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Empezar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(302, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Contador de Partidos";
+            // 
+            // txt2
+            // 
+            txt2.Location = new Point(311, 75);
+            txt2.Name = "txt2";
+            txt2.Size = new Size(100, 23);
+            txt2.TabIndex = 2;
+            txt2.TextChanged += textBox2_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(118, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(110, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Nombre del Equipo";
+            label1.Click += label1_Click;
+            // 
+            // txt1
+            // 
+            txt1.Location = new Point(128, 75);
+            txt1.Name = "txt1";
+            txt1.Size = new Size(100, 23);
+            txt1.TabIndex = 0;
+            txt1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(grp1);
+            Name = "Form1";
+            Text = "Form1";
+            grp1.ResumeLayout(false);
+            grp1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox grp1;
+        private Label label1;
+        private TextBox txt1;
+        private Label label2;
+        private TextBox txt2;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+    }
+}
